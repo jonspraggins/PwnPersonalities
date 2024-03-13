091edea4d247627e7156f5b673d26cf696b111eb
Jon Spraggins <jonspraggins@users.noreply.github.com>
2024-03-13 10:27:20 -0500

    Create voice.py

---
+++ PwnQueen/voice.py
@@ -0,0 +1,207 @@
+import gettext
+import os
+import random
+
+###
+# Voice: PwnQueen
+# Description: RuPaul's Drag Race meets Pwnagotchi.  Sassy quotes from the many contestants from the show, ready to glamour up your pwning game.
+###
+
+class Voice:
+    def __init__(self, lang):
+        localedir = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'locale')
+        translation = gettext.translation(
+            'voice', localedir,
+            languages=[lang],
+            fallback=True,
+        )
+        translation.install()
+        self._ = translation.gettext
+
+    def custom(self, s):
+        return s
+
+    def default(self):
+        return self._('ZzzzZZzzzzZzzz')
+
+    def on_starting(self):
+        return random.choice([
+            self._('Hi, I\'m Pwnagotchi! Starting ...'),
+            self._('New day, new hunt, new pwns!'),
+            self._('Hack the Planet!')])
+
+    def on_ai_ready(self):
+        return random.choice([
+            self._('AI ready... to SLAY!'),
+            self._('The neural network is ready.')])
+
+    def on_keys_generation(self):
+        return random.choice([
+            self._('Generating keys, do not turn off ...')])
+
+    def on_normal(self):
+        return random.choice([
+            '',
+            '...'])
+
+    def on_free_channel(self, channel):
+        return self._('{channel}, you\’re free now, you don\’t have to rock the yard sandals.').format(channel=channel)
+
+    def on_reading_logs(self, lines_so_far=0):
+        if lines_so_far == 0:
+            return self._('Reading last session logs ...')
+        else:
+            return self._('Read {lines_so_far} log lines so far ...').format(lines_so_far=lines_so_far)
+
+    def on_bored(self):
+        return random.choice([
+            self._('I\'m bored ...'),
+            self._('Let\'s go for a walk!')])
+
+    def on_motivated(self, reward):
+        return self._('This is the best day of my life!')
+
+    def on_demotivated(self, reward):
+        return self._('Not today, Satan. Not today.')
+
+    def on_sad(self):
+        return random.choice([
+            self._('The level of unprofessionalism....far too much!'),
+            self._('I feel like you\’re being sabotaged by your inner saboteur.'),
+            self._('I feel very attacked right now.'),
+            '...'])
+
+    def on_angry(self):
+        # passive aggressive or not? :D
+        return random.choice([
+            '...',
+            self._('Go back to Party City where you belong!'),
+            self._('I\'m mad at you!')])
+
+    def on_excited(self):
+        return random.choice([
+            self._('YAAS QUEEN!'),
+            self._('Because I am what? Sickening!'),
+            self._('Check your lipstick before you come for me.'),
+            self._('I have a master\’s degree in FIERCE.'),
+            self._('I could never be a Kardashian because I have talent.')])
+
+    def on_new_peer(self, peer):
+        if peer.first_encounter():
+            return random.choice([
+                self._('{name}! Lip-sync for your LIFE!').format(name=peer.name())])
+        else:
+            return random.choice([
+                self._('{name}! You\’re perfect, you\’re beautiful, you look like Eva Evangelista.').format(name=peer.name()),
+                self._('Hey {name} how are you doing?').format(name=peer.name()),
+                self._('Unit {name} is nearby!').format(name=peer.name())])
+
+    def on_lost_peer(self, peer):
+        return random.choice([
+            self._('Uhm ... goodbye {name}').format(name=peer.name()),
+            self._('{name} is gone ...').format(name=peer.name())])
+
+    def on_miss(self, who):
+        return random.choice([
+            self._('Whoops ... {name} is gone.').format(name=who),
+            self._('{name} missed!').format(name=who),
+            self._('Missed!')])
+
+    def on_grateful(self):
+        return random.choice([
+            self._('Good friends are a blessing!'),
+            self._('I love my friends!')])
+
+    def on_lonely(self):
+        return random.choice([
+            self._('Nobody wants to play with me ...'),
+            self._('I feel so alone ...'),
+            self._('Where\'s everybody?!')])
+
+    def on_napping(self, secs):
+        return random.choice([
+            self._('Napping for {secs}s ...').format(secs=secs),
+            self._('Zzzzz'),
+            self._('ZzzZzzz ({secs}s)').format(secs=secs)])
+
+    def on_shutdown(self):
+        return random.choice([
+            self._('Good night.'),
+            self._('Zzz')])
+
+    def on_awakening(self):
+        return random.choice(['...', '!'])
+
+    def on_waiting(self, secs):
+        return random.choice([
+            self._('Waiting for {secs}s ...').format(secs=secs),
+            '...',
+            self._('Looking around ({secs}s)').format(secs=secs)])
+
+    def on_assoc(self, ap):
+        ssid, bssid = ap['hostname'], ap['mac']
+        what = ssid if ssid != '' and ssid != '<hidden>' else bssid
+        return random.choice([
+            self._('{what}, the library is now open!').format(what=what),
+            self._('Where did you get bread, {what}?').format(what=what),
+            self._('{what}, dance for your LIFE!').format(what=what)])
+
+    def on_deauth(self, sta):
+        return random.choice([
+            self._('Just decided that {mac} needs no WiFi!').format(mac=sta['mac']),
+            self._('Your tone seems very pointed right now, {mac}').format(mac=sta['mac']),
+            self._('Sashay away, {mac}!').format(mac=sta['mac'])])
+
+    def on_handshakes(self, new_shakes):
+        s = 's' if new_shakes > 1 else ''
+        return self._('Cool, we got {num} new handshake{plural}!').format(num=new_shakes, plural=s)
+
+    def on_unread_messages(self, count, total):
+        s = 's' if count > 1 else ''
+        return self._('You have {count} new message{plural}!').format(count=count, plural=s)
+
+    def on_rebooting(self):
+        return self._("Oops, something went wrong ... Rebooting ...")
+
+    def on_uploading(self, to):
+        return self._("Uploading data to {to} ...").format(to=to)
+
+    def on_last_session_data(self, last_session):
+        status = self._('Kicked {num} stations\n').format(num=last_session.deauthed)
+        if last_session.associated > 999:
+            status += self._('Made >999 new friends\n')
+        else:
+            status += self._('Made {num} new friends\n').format(num=last_session.associated)
+        status += self._('Got {num} handshakes\n').format(num=last_session.handshakes)
+        if last_session.peers == 1:
+            status += self._('Met 1 peer')
+        elif last_session.peers > 0:
+            status += self._('Met {num} peers').format(num=last_session.peers)
+        return status
+
+    def on_last_session_tweet(self, last_session):
+        return self._(
+            'I\'ve been pwning for {duration} and kicked {deauthed} clients! I\'ve also met {associated} new friends and ate {handshakes} handshakes! #pwnagotchi #pwnlog #pwnlife #hacktheplanet #skynet').format(
+            duration=last_session.duration_human,
+            deauthed=last_session.deauthed,
+            associated=last_session.associated,
+            handshakes=last_session.handshakes)
+
+    def hhmmss(self, count, fmt):
+        if count > 1:
+            # plural
+            if fmt == "h":
+                return self._("hours")
+            if fmt == "m":
+                return self._("minutes")
+            if fmt == "s":
+                return self._("seconds")
+        else:
+            # sing
+            if fmt == "h":
+                return self._("hour")
+            if fmt == "m":
+                return self._("minute")
+            if fmt == "s":
+                return self._("second")
+        return fmt
